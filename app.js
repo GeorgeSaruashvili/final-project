b644013698c7d33a3d106edb67772825ff1eed78
GeorgeSaruashvili <giorgisaruashvili3@gmail.com>
2025-12-23 11:22:12 +0400

    photo slider with js

---
+++ app.js
@@ -0,0 +1,9 @@
+//photo slider
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+setInterval(() => {
+  slides[current].classList.remove("active");
+  current = (current + 1) % slides.length;
+  slides[current].classList.add("active");
+}, 5000);
