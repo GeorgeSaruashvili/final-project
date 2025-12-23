43f06688c0346c5c710344226ecbd3605489a58b
GeorgeSaruashvili <giorgisaruashvili3@gmail.com>
2025-12-23 13:37:03 +0400

    about me section with progress bar

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ setInterval(() => {
   current = (current + 1) % slides.length;
   slides[current].classList.add("active");
 }, 5000);
+
+//progress bar
+const aboutSection = document.querySelector("#about");
+const progressBars = document.querySelectorAll(".progress-bar");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        progressBars.forEach((bar) => {
+          const value = bar.getAttribute("data-progress");
+          bar.style.width = value + "%";
+        });
+        observer.unobserve(aboutSection);
+      }
+    });
+  },
+  {
+    threshold: 0.4,
+  }
+);
+
+observer.observe(aboutSection);
